f51e36203d6481ec96760e686b06f123c9d4744e
James Richards <heiho1@jamess-mbp.attlocal.net>
2021-06-05 23:22:13 -0400

    compiling of initial ERC 721 prototypes

---
+++ contracts/Comp.sol
@@ -0,0 +1,17 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity >=0.7.0 <=0.8.4;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+import "hardhat/console.sol";
+
+contract Comp is ERC721("Compromised", "COMP"), ERC721Enumerable {
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId) override(ERC721, ERC721Enumerable) internal {
+      console.log("_beforeTokenTransfer '%s' to '%s'", from, to);
+    }
+
+    function supportsInterface(bytes4 interfaceId) public view override(ERC721, ERC721Enumerable) returns (bool) {
+        return ERC721.supportsInterface(interfaceId);
+    }
+}
